77ebd86b443c65b4ac7b30ccd8ee17498cbba017
Jonas Wielicki <j.wielicki@sotecware.net>
2012-04-11 18:07:24 +0200

    Work on tables

---
+++ Evaluation/Table.py
@@ -0,0 +1,145 @@
+# encoding=utf-8
+from __future__ import unicode_literals, division, print_function
+
+import abc
+import itertools
+
+import sympy.physics.units as units
+
+class Identity(object):
+    """
+    Used as a magic value for bypassing the operation of MapColumn objects.
+    """
+
+class TableColumn(object):
+    """
+    Represents a column in a :class:`Table`.
+
+    *symbol* must be a sympy :class:`sympy.core.symbol.Symbol` which identifies
+    the value in the column. *title* can be set if the string representation of
+    *symbol* is not suitable as a title for the column.
+
+    *displayUnit* must either be a string which can be evaluated to a sympy
+    expression using `eval(displayUnit, sympy.physics.units.__dict__)` or a
+    container like `(unitName, unitExpr)`, with *unitName* being the plain text
+    name of the unit and *unitExpr* being a sympy expression resembling the
+    unit.
+
+    *defaultMagnitude* can be a unitless sympy expression which is then used to
+    scale the final quantities. If this is `None`, automatic scaling takes
+    place.
+
+    One can create an iterable over the rows of a table column by calling
+    :func:`iter` on a :class:`TableColumn` instance.
+    
+    You may override the :func:`TableColumn.dataHash` method to reflect changes
+    in the data stored in the column. This helps for caching calculated data.
+    You may return a value of arbitary type, but it must be hashable and
+    comparable. If you do not the columns contents to be cached, return
+    `NotImplemented`.
+    """
+
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self, symbol, displayUnit, defaultMagnitude=None, title=None, **kwargs):
+        super(TableColumn, self).__init__(**kwargs)
+        self.symbol = symbol
+        self.title = title or unicode(symbol)
+        try:
+            if isinstance(displayUnit, (unicode, str)):
+                raise TypeError()  # ugly, but avoids code duplication
+            self.unit, self.unitExpr = displayUnit
+        except TypeError:
+            self.unit = displayUnit
+            self.unitExpr = eval(unicode(displayUnit), units.__dict__)
+        if defaultMagnitude is None:
+            raise NotImplementedError("Cannot scale automagically yet.")
+        self.magnitude = defaultMagnitude
+
+    @abc.abstractmethod
+    def __iter__(self):
+        pass
+
+    @abc.abstractmethod
+    def __len__(self):
+        pass
+
+    def dataHash(self):
+        return NotImplemented
+
+    def iterDisplay(self):
+        unitName = self.unit
+        unify = 1 / (self.unitExpr * self.magnitude)
+        for row in self:
+            yield (row * unify, unitName)
+
+class MapColumn(TableColumn):
+    def __init__(self, symbol, unit, operation, defaultMagnitude=None,
+            title=None, **kwargs):
+        super(MapColumn, self).__init__(self, symbol, unit,
+            defaultMagnitude=defaultMagnitude, title=title, **kwargs)
+        if not hasattr(operation, "__call__"):
+            raise TypeError("operation must be callable.")
+        self.operation = operation if operation is not 
+
+    def _mapData(self, data):
+        if self.operation is Identity:
+            return data
+        return itertools.imap(self.operation, data)
+
+class DataColumn(MapColumn):
+    """
+    Implements a raw data column.
+
+    *data* must be a iterable of sympy expressions resembling the data
+    including its units. The iterable will only be evaluated once, mapped with
+    *operation* and stored as list internally.
+    """
+    def __init__(self, symbol, unit, data, operation=Identity,
+            defaultMagnitude=None, title=None, **kwargs):
+        super(DataColumn, self).__init__(symbol, unit, operation,
+            defaultMagnitude=defaultMagnitude, title=title, **kwargs)
+        self.data = list(self._mapData(data))
+
+    def __iter__(self):
+        return iter(self.data)
+
+    def __len__(self):
+        return len(self.data)
+
+    def dataHash(self):
+        return self.data
+
+class CachedColumn(TableColumn):
+    def __init__(self, symbol, unit, referenceColumns, **kwargs):
+        super(CachedColumn, self).__init__(symbol, unit, **kwargs)
+        self.referenceColumns = frozenset(referenceColumns)
+
+    def cacheValid(self):
+
+class DerivatedColumn(TableColumn):
+    def __init__(self, symbol, unit, sources, sympyExpr, defaultMagnitude=None,
+            title=None, **kwargs):
+        super(DerivatedColumn, self).__init__(symbol, unit, operation,
+            defaultMagnitude=defaultMagnitude, title=title, **kwargs)
+
+        self.sources = frozenset(sources)
+
+    def __iter__
+
+class Table(object):
+    """
+    Maintains a measurement table representation.
+
+    *title* is used as a title whenever a document representation is created
+    from the table object. Some backends may also support *caption* for a more
+    detailed description.
+
+    *caption* defaults to *title*.
+    """
+    
+    def __init__(self, title, caption=None, **kwargs):
+        super(Table, self).__init__(**kwargs)
+        self.title = title
+        self.caption = caption
+        
