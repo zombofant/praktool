620a180a701f32563f655c56278c0a13170ce9b3
Sebastian Riese <sebastian.riese.mail@web.de>
2012-04-12 13:40:55 +0200

    CVS parsing support

--- Evaluation/TableParser.py
+++ Evaluation/TableParser.py
@@ -12,8 +12,39 @@ import Table
 class Error(Exception):
     pass
 
-def ParseCSV(data, cols):
+def ParseCSV(data, cols=None, dialect=None):
+    """
+    Parse a data file in the csv format.
 
+    *data* is an iterable returning the lines of the file.
+
+    If *cols* is not `None`, it has to be an sequence of
+    :class:`Table.DataColumns`, if it is `None` the columns are
+    generated from the first row of the file.
+
+    *dialect* is passed to the `csv.reader` constructor as dialect
+     argument.
+    """
+    # only handle the first line different if no cols are given
+    first = cols is None
+    for fields in csv.reader(lines, dialect=dialect):
+        if first:
+            for field in fields:
+                name, unit = field.split('/', 1)
+                cols.append(Table.DataColumn(sympy.symbol(name), unit, []))
+        else:
+            if len(fields) != len(cols):
+                raise Error('Invalid Table: Incorrect number of columns')
+
+            for col, field in zip(cols, fields):
+                try:
+                    dataItemAsNumber = int(field)
+                except ValueError:
+                    dataItemAsNumber = float(field)
+
+                col.appendRow(dataItemAsNumber * col.unitExpr)
+
+        first = False
 
 def ParseGnuplot(data, cols=None, annotation='%', header_sep=None):
     """
