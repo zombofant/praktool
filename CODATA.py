0c77b7248f7ed2d29a90f67c43588edd2f2a9001
Jonas Wielicki <j.wielicki@sotecware.net>
2012-08-30 14:14:23 +0200

    Add some constants to CODATA module

--- CODATA.py
+++ CODATA.py
@@ -9,6 +9,8 @@ e = u.eV / u.V
 me = 910938215*u.kg*1/(10**39)
 mp = 1672621637*u.kg*1/(10**36)
 rydberg = ((e**2/(4*sp.pi*u.electric_constant))**2*(2*sp.pi**2*me/(u.planck**3*u.speed_of_light)))
+planck2pi = u.planck / (2*sp.pi)
+rydberg_energy = e**4*me/(2*(4*sp.pi*u.electric_constant*planck2pi)**2)
 
 def patchUnits(u):
     def patchUnits():
@@ -16,6 +18,8 @@ def patchUnits(u):
         u.me = me
         u.mp = mp
         u.rydberg = rydberg
+        u.planck2pi = planck2pi
+        u.rydberg_energy = rydberg_energy
     return patchUnits
 patchUnits = patchUnits(u)
 
