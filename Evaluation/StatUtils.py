32070e0786b88d0363e17dcac5ce15a632880054
Jonas Wielicki <j.wielicki@sotecware.net>
2012-12-08 20:13:24 +0100

    Add function to do a single-value error propagation

--- Evaluation/StatUtils.py
+++ Evaluation/StatUtils.py
@@ -1,3 +1,7 @@
+from __future__ import print_function
+
+__all__ = ["mean", "propagate_eval"]
+
 import sympy as sp
 
 def mean(data):
@@ -35,5 +39,39 @@ def buildErrorExpression(expr, symbols):
     """
     diffs = 0
     for symbol, dsymbol in symbols:
-        diffs += (sp.diff(expr.subs(symbol, symbol), symbol) * dsymbol)**2
+        diffs += (sp.diff(expr, symbol) * dsymbol)**2
     return sp.sqrt(diffs)
+
+def propagate_eval(expr, values):
+    """
+    Return the result of *expr* with *values* and the result of the gaussian
+    error propagation.
+
+    *values* must be an iterable of tuples, each consisting of the symbol
+    associated with the value, the value itself, the error to be propagated
+    (can be zero) and an expression which resembles the unit of the value (can
+    be 1).
+
+    The return value is a tuple of the value gained from evaluating the
+    expression itself and the error gained from applying gaussian error
+    propagation using the given error values.
+    """
+    value_sub = [(symb, value*unit) for symb, value, _, unit in values]
+    error_symbols = [sp.Dummy(b"delta_error_propagation_"+str(symb))
+                     for symb, _, _, _ in values]
+    symbol_map = [(symb, err_symb)
+                  for err_symb, (symb, _, _, _) in zip(error_symbols, values)]
+    error_sub = [(error_symb, error*unit)
+                 for error_symb, (_, _, error, unit) in zip(error_symbols, values)]
+
+    result_value = expr.subs(value_sub)
+
+    error_expr = buildErrorExpression(
+        expr,
+        symbol_map
+    )
+
+    error_value = error_expr.subs(value_sub).subs(error_sub)
+
+    return result_value, error_value
+
