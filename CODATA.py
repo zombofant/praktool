f5a33451a76825bf4108bf328bad00d9c3ad5e87
Jonas Wielicki <j.wielicki@sotecware.net>
2012-12-08 20:13:36 +0100

    Add mass of neutron

--- CODATA.py
+++ CODATA.py
@@ -8,6 +8,7 @@ import sympy as sp
 e = u.eV / u.V
 me = 910938215*u.kg*1/(10**39)
 mp = 1672621637*u.kg*1/(10**36)
+mn = 1674927351*u.kg*1/(10**36)
 rydberg = ((e**2/(4*sp.pi*u.electric_constant))**2*(2*sp.pi**2*me/(u.planck**3*u.speed_of_light)))
 planck2pi = u.planck / (2*sp.pi)
 rydberg_energy = e**4*me/(2*(4*sp.pi*u.electric_constant*planck2pi)**2)
@@ -17,10 +18,11 @@ def patchUnits(u):
         u.e = e
         u.me = me
         u.mp = mp
+        u.mn = mn
         u.rydberg = rydberg
         u.planck2pi = planck2pi
         u.rydberg_energy = rydberg_energy
     return patchUnits
 patchUnits = patchUnits(u)
 
-del u
+del u, sp
